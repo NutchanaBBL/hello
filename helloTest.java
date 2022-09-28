0fa80f3ef81b9fa9266eb9b5217abe259c9e0811
NutchanaBBL <114470361+NutchanaBBL@users.noreply.github.com>
2022-09-28 16:31:50 +0700

    Update helloTest.java
    update code final

--- helloTest.java
+++ helloTest.java
@@ -1,6 +1,6 @@
 public class Hello(){
   public static void main(String[] args){
     system.out.print("hello world");
-    system.out.print("test connect jenkins111111");
+    system.out.print("test connect jenkins_final");
   }
 }
